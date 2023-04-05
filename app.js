49513d622646729d2be65d147a9ef034f4ecec16
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-04-05 14:52:38 +0100

    Process of adding the functionality

--- app.js
+++ app.js
@@ -4,6 +4,47 @@ for (let i = 0; i < myNodeList.length; i++) {
 	var span = document.createElement("SPAN");
 	var txt = document.createTextNode("\u00D7");
 	span.className = "close";
-    span.appendChild(txt);
-    myNodeList[i].appendChild(span)
+	span.appendChild(txt);
+	myNodeList[i].appendChild(span);
+}
+
+var close = document.getElementsByTagName("close");
+var i;
+for (let i = 0; i < close.length; i++) {
+	close[i].onClick = function () {
+		var div = this.parentElement;
+		div.style.display = "none";
+	};
+}
+
+var list = document.querySelector("ul");
+list.addEventListener("click", function (ev) {
+	if (ev.target.tagName === "LI") {
+		ev.target.classList.toggle("checked");
+	}
+});
+
+function newElement() {
+	var li = document.createElement("LI");
+	var inputValue = document.getElementById("myInput").value;
+	var t = document.createTextNode(inputValue);
+	li.appendChild(t);
+	if (inputValue === "") {
+		alert("You must write something!");
+	} else {
+		document.getElementById("myUL").appendChild(li);
+	}
+
+	document.getElementById("myInput").value = "";
+	var span = document.createElement("SPAN");
+	var txt = document.createTextNode("U00D7");
+	span.appendChild(txt);
+    li.appendChild(span);
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none"
+        }
+    } 
+    
 }
